416a46eb72bbe68093663b0c8fdb56d9773e7e81
theseusyang <theseusyang@gmail.com>
2021-09-23 19:58:30 +0800

    add apache license

--- access-service/src/main/java/com/huahui/datasphere/portal/dao/impl/RoleDaoImpl.java
+++ access-service/src/main/java/com/huahui/datasphere/portal/dao/impl/RoleDaoImpl.java
@@ -974,7 +974,7 @@ public class RoleDaoImpl extends BaseDAOImpl implements RoleDao {
      * Drop all resources.
      */
     @Override
-    public void dropResources(SecuredResourceCategory... categories) {
+    public void dropResources(SecuredResourceCategoryInf... categories) {
 
         boolean hasMetaModel = false;
         if (ArrayUtils.isEmpty(categories)) {
